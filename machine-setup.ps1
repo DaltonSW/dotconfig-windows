4148003704e5ef14f1a39cbba8857ed225dc566f
Dalton <daltonstwilliams@gmail.com>
2024-01-27 12:25:31 -0600

    Updated README, included winget scripts, and some oh-my-posh themes

---
+++ machine-setup.ps1
@@ -0,0 +1,7 @@
+winget install Microsoft.WindowsTerminal`Microsoft.VisualStudioCode Microsoft.PowerToys
+winget install voidtools.Everything WinDirStat.WinDirStat
+winget install Nilesoft.Shell
+winget install 9PGCV4V3BK4W
+winget install glzr-io.glazewm
+winget install JanDeDobbeleer.OhMyPosh -s winget
+winget install GitHub.GitHubDesktop GitHub.cli
