3d7a1c8be2442e340b7fb940533e15ebdf5ccde7
Dalton <daltonstwilliams@gmail.com>
2024-01-27 12:53:29 -0600

    Updated install script

--- machine-setup.ps1
+++ machine-setup.ps1
@@ -1,8 +1,15 @@
-winget install Microsoft.WindowsTerminal`Microsoft.VisualStudioCode Microsoft.PowerToys
-winget install voidtools.Everything WinDirStat.WinDirStat
-winget install Nilesoft.Shell
-winget install 9PGCV4V3BK4W
-winget install glzr-io.glazewm
-winget install JanDeDobbeleer.OhMyPosh -s winget
-winget install GitHub.GitHubDesktop GitHub.cli
-winget install JetBrains.Toolbox
+winget install --id=Microsoft.WindowsTerminal --accept-package-agreements --accept-source-agreements
+winget install --id=Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+winget install --id=Microsoft.PowerToys --accept-package-agreements --accept-source-agreements
+winget install --id=voidtools.Everything --accept-package-agreements --accept-source-agreements
+winget install --id=WinDirStat.WinDirStat --accept-package-agreements --accept-source-agreements
+winget install --id=Nilesoft.Shell --accept-package-agreements --accept-source-agreements
+winget install --id=9PGCV4V3BK4W -s msstore --accept-package-agreements --accept-source-agreements
+winget install --id=glzr-io.glazewm --accept-package-agreements --accept-source-agreements
+winget install --id=JanDeDobbeleer.OhMyPosh -s winget --accept-package-agreements --accept-source-agreements
+winget install --id=GitHub.GitHubDesktop --accept-package-agreements --accept-source-agreements
+winget install --id=GitHub.cli --accept-package-agreements --accept-source-agreements
+winget install --id=JetBrains.Toolbox --accept-package-agreements --accept-source-agreements
+winget install --id=Spotify.Spotify --accept-package-agreements --accept-source-agreements
+winget install --id=Discord.Discord --accept-package-agreements --accept-source-agreements
+winget install --id=Valve.Steam --accept-package-agreements --accept-source-agreements
